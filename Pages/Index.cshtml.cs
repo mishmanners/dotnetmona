5f1d9cf14e28965fcb876590dec4fb331b626b6d
MishManners <36594527+MishManners@users.noreply.github.com>
2020-05-19 20:21:56 +1000

    Initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using dotnetweb;
+
+namespace dotnetweb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string[] Messages {
+            get {
+                return new string[] {
+                    "Hello Build!",
+                    "Would you like a Cup<T>?",
+                    ".NET all the things!",
+                    "Would you like to play a game?"
+                };
+            }
+        }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
